c2518f3539b917ff805651b6b92d386bf766b1db
Guilherme Brennand <g.brennand@hotmail.com>
2020-09-15 16:15:20 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -3,6 +3,7 @@ package com.devsuperior.dspesquisa.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
